8dc1e296e64bcbf19c9be19d6b5f32585ccbd959
Guna Sekhar Dora <gunasekhar.dora3@gmail.com>
2020-06-21 12:23:25 +0530

    feat: initialize spring-boot

---
+++ src/main/java/org/kgsd/capitals/CapitalsCamelApplication.java
@@ -0,0 +1,13 @@
+package org.kgsd.capitals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitalsCamelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapitalsCamelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/kgsd/capitals/CapitalsCamelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kgsd.capitals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CapitalsCamelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
